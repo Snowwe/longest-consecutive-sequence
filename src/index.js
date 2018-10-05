cff8035e9621ec14f772f6b1b9355b0947d755c9
Snezhana <snowwe25@rambler.ru>
2018-10-05 18:43:28 +0300

    version_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let maxCount = 0
+        , tempCount = 1;
+    array.sort((a, b) => a - b);
+
+    for (let i = 0, len = array.length; i < len; ++i) {
+        if (i === 0)
+            ++maxCount;
+
+        else if (array[i] === (array[i - 1] + 1)) {
+            ++tempCount;
+            if (maxCount < tempCount)
+                maxCount = tempCount;
+        } else if (array[i] === array[i - 1]) {
+            continue;
+        } else
+            tempCount = 1;
+    }
+
+    return maxCount;
 }
+;
